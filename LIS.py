2a4b4bdee7c0edc40ef219787c87e16d7b969af6
zhangzhenhua <zzh.matrix@gmail.com>
2016-04-05 22:49:35 +0800

    nice

---
+++ LIS.py
@@ -0,0 +1,41 @@
+def lis(aList, pre):
+    d = [0] * len(aList)
+    longest = 1
+    global longestPoint
+
+    for p, n in enumerate(aList):
+        pre[p] = n
+
+    for i in range(0, len(aList)):
+        d[i] = 1
+        for j in range(0, i):
+            if aList[i] >= aList[j] and (d[j] + 1) > d[i]:
+                d[i] = d[j] + 1
+                pre[i] = j
+        if d[i] > longest:
+            longest = d[i]
+            longestPoint = i
+    return longest
+
+aList = [5, 3, 4, 8, 6, 7]
+pre = [0] * len(aList)
+longestPoint = 0
+
+print("result:", lis(aList, pre))
+
+print("longestPoint:", longestPoint)
+print(aList)
+print(pre)
+
+point = longestPoint
+
+resultList = list([])
+
+resultList.append(aList[point])
+while pre[point] != aList[point]:
+    point = pre[point]
+    resultList.append(aList[point])
+
+while len(resultList) > 1:
+    print(resultList.pop(), end="->")
+print(resultList.pop())
